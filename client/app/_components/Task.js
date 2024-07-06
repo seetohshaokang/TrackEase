b70bbe778b45d4ceec7f7623db058a0ee84fc39a
Shao Kang Seetoh <shaokangseetoh@Shaos-MacBook-Air-2.local>
2024-07-06 12:12:41 +0800

    refactored bookmark task component

--- client/app/_components/Task.js
+++ client/app/_components/Task.js
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { TaskContext } from "../context/TaskContext";
+import BookmarkTaskButton from "./BookmarkTaskButton";
 import DeleteTaskButton from "./DeleteTaskButton";
 import UpdateTaskForm from "./UpdateTaskForm";
 
@@ -10,27 +11,6 @@ export default function Task({ task, onTaskChange }) {
   const [deadline, setDeadline] = useState(task.deadline || "");
   const [remarks, setRemarks] = useState(task.remarks || "");
   const [tags, setTags] = useState(task.tags || []);
-
-  async function handleBookmark() {
-    const token = localStorage.getItem("firebaseToken");
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/tasks/bookmark/${task._id}`,
-        {
-          method: "PUT",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Failed to bookmark task");
-      }
-      onTaskChange();
-    } catch (error) {
-      console.error("Error bookmarking task", error);
-    }
-  }
 
   async function handleComplete() {
     const token = localStorage.getItem("firebaseToken");
@@ -104,43 +84,11 @@ export default function Task({ task, onTaskChange }) {
               </svg>
             </button>
             <DeleteTaskButton taskId={task._id} onTaskChange={onTaskChange} />
-            <button
-              onClick={handleBookmark}
-              className="btn btn-accent text-white btn-sm"
-              title={task.bookmarked ? "Un-Bookmark" : "Bookmark"}
-            >
-              {task.bookmarked ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-                  />
-                </svg>
-              )}
-            </button>
+            <BookmarkTaskButton
+              taskId={task._id}
+              bookmarked={task.bookmarked}
+              onTaskChange={onTaskChange}
+            />
             <button
               onClick={handleComplete}
               className="btn btn-success text-white btn-sm"
