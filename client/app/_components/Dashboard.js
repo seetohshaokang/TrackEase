2e367d2b3196ddbfaa9f41fda398f7ceffee9bee
Shao Kang Seetoh <shaokangseetoh@Shaos-MacBook-Air-2.local>
2024-07-24 22:45:07 +0800

    updated weekly dashboard

--- client/app/_components/Dashboard.js
+++ client/app/_components/Dashboard.js
@@ -3,7 +3,7 @@ import WeeklyTaskList from "./WeeklyTaskList";
 function Dashboard() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen  bg-gray-100">
-      <h1 className="text-3xl font-bold underline mb-5"> Dashboard </h1>
+      <h1 className="text-3xl font-bold underline mb-5"> Weekly Dashboard </h1>
       <WeeklyTaskList />
     </div>
   );
