d4b2a79ad6053ecb73466d7abe44291a80496edb
LegendarySnowman <161958895+seetohshaokang@users.noreply.github.com>
2025-01-17 17:23:02 +0800

    Delete server/config directory

--- server/config/googleAPI.js
+++
@@ -1,9 +0,0 @@
-const { google } = require("googleapis");
-
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/auth/callback"
-);
-
-module.exports = oauth2Client;
